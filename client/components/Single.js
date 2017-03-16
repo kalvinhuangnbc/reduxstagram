52c14c6a9747578753f6355a6082a804b0c128a6
Kalvin Huang <kalvin.huang@nbcuni.com>
2017-03-16 14:53:30 -0700

    Build router for Photogrid and Single

---
+++ client/components/Single.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+const Single = React.createClass({
+  render() {
+    return (
+      <div ClassName="single-photo">
+        Single Grid
+      </div>
+    )
+  }
+})
+
+export default Single;

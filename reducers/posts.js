95cdc5b2641ea1e95f170218ecf31ced0b3b6ba1
Kalvin Huang <kalvin.huang@nbcuni.com>
2017-03-16 15:58:08 -0700

    Add posts and comments reducers

---
+++ reducers/posts.js
@@ -0,0 +1,15 @@
+// reducer takes in 2 things:
+
+// 1. the action (info about what happened)
+// 2. copy of current state
+
+// comes in: action, store (state)
+// ok, let me see
+// return updated store
+
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
+}
+
+export default posts;

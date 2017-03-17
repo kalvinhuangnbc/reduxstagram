265cb6b488083dc550af1b96e0ed550b698ed933
Kalvin Huang <kalvin.huang@nbcuni.com>
2017-03-17 11:34:57 -0700

    Displaying and adding comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
 const Comments = React.createClass({
-  render() {
+  renderComment(comment, i) {
     return (
-      <div className="comment">
-        Im a comment!!!
+      <div className="comment" key={i}>
+        <p>
+          <strong>{comment.user}</strong>
+          {comment.text}
+          <button className="remove-comment">&times;</button>
+        </p>
+      </div>
+    )
+  },
+
+  render() {
+    console.log(this);
+    return (
+      <div className="comments">
+        {this.props.postComments.map(this.renderComment)}
+        <form ref="commentForm" className="comment-form">
+          <input type="test" ref="author" placeholder="author"/>
+          <input type="test" ref="comment" placeholder="comment"/>
+          <input type="submit" hidden />
+        </form>
       </div>
     )
   }

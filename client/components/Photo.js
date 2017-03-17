c36c052455dde79642066557b3571072960c5786
Kalvin Huang <kalvin.huang@nbcuni.com>
2017-03-16 17:51:45 -0700

    Displaying redux state inside Photo component

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -1,10 +1,19 @@
 import React from 'React';
+import { Link } from 'react-router';
 
 const Photo = React.createClass({
   render() {
+    // so we dont have to type this.props every time for these variables
+    const { post, i, comments } = this.props;
+
     return (
       <figure className="grid-figure">
-        I am a photo!
+        <div className="grid-photo-wrap">
+          <Link to={`/view/${post.code}`}>
+            <img src={post.display_src} alt={post.caption}
+              className="grid-photo" />
+          </Link>
+        </div>
       </figure>
     )
   }
